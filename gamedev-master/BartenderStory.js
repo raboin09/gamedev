7257dafa2311796f6e8904b11fdf501270bc8210
raboin09 <raboin09@gmail.com>
2017-02-28 10:53:11 -0600

    Delete BartenderStory.js

--- gamedev-master/BartenderStory.js
+++
@@ -1,9 +0,0 @@
-function bartenderPart1() {
-    displayText = new createjs.Text("Hi, I'm the bartender.", "15px Helvetica", "#fff");
-    displayText.x = bartender.x + 15;
-    displayText.y = bartender.y - 15;
-    stage.addChild(displayText);
-    displayText = true;
-    displayTextBool = true;
-
-}
